d72450bcc410ddb540c5062924797899cdf112f3
hebs <hebs007wrx@gmail.com>
2018-04-07 05:26:31 +0000

    user model added and basic form created

--- server/src/app.js
+++ server/src/app.js
@@ -2,17 +2,15 @@ const express = require('express')
 const bodyParser = require('body-parser')
 const cors = require('cors')
 const morgan = require('morgan')
-
+const {sequelize} = require('./models')
+const config = require('./config/config')
 const app = express()
 app.use(morgan('combined'))
 app.use(bodyParser.json())
 app.use(cors())
-
-app.post('/register', (req, res) => {
-  res.send({
-    message: `Your account has been created.`
+require('./routes')(app)
+sequelize.sync()
+  .then(() => {
+    app.listen(config.port)
+    console.log(`Server started on port ${config.port}`)
   })
-})
-
-app.listen(process.env.PORT || 8082)
-console.log('Express server running on port 8082!')
